9eac681f2991924b63eedafacece61fe7b275345
ramedina98 <b_stone99@outlook.es>
2024-04-25 18:37:23 -0600

    ready to run in railway

--- server.js
+++ server.js
@@ -7,10 +7,14 @@ const app = express();
 //configure express to server static files...
 app.use("/static", express.static(path.resolve(__dirname, "frontend", "static")));
 //we respond wiht the index file...
-app.get('/*', (req, res) => {
+app.get('/*', (_req, res) => {
     res.sendFile(path.resolve(__dirname,"frontend","index.html"));
 });
 
-app.listen(5000, () => {
-    console.log('Server up running in http://localhost:5000/')
+// get the port number from enviroment variable or use the default port...
+const PORT = process.env.PORT || 3000; 
+
+//Start the server and listen on the specified port...
+app.listen(PORT, "0.0.0.0", () => {
+    console.log(`Server up running in http://localhost:${PORT}/`)
 });
